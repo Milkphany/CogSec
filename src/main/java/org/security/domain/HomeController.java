5983cd582915509b984d8c98f312481d846b1351
milkphany <milkphany@gmail.com>
2013-12-04 18:12:36 -0500

    Authentication should now work.

--- src/main/java/org/security/domain/HomeController.java
+++ src/main/java/org/security/domain/HomeController.java
@@ -9,9 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/")
@@ -65,8 +68,10 @@ public class HomeController {
         return "/home/login";
     }
 
-    @RequestMapping(value ="login2",method = RequestMethod.GET)
-    public String getNextLogin(ModelMap modelMap) {
+    @RequestMapping(value ="login2",method = RequestMethod.POST)
+    public String getNextLogin(ModelMap modelMap, HttpServletRequest request) {
+
+        modelMap.addAttribute("username", request.getParameter("username"));
 
         return "/home/login2";
     }
