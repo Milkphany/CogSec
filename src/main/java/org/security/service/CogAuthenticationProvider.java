ac41434ebea1c4f9ce126b7ee825ab83ce214f41
Jason Chen <milkphany@gmail.com>
2013-12-11 03:25:41 -0500

    Fixed logic

--- src/main/java/org/security/service/CogAuthenticationProvider.java
+++ src/main/java/org/security/service/CogAuthenticationProvider.java
@@ -39,27 +39,6 @@ public class CogAuthenticationProvider implements AuthenticationProvider {
             throw new BadCredentialsException("Username not found");
         if (account.isLoggedIn() && !account.getUsername().equalsIgnoreCase("milky"))
             throw new BadCredentialsException("You have been able to successfully log in! You should get an email from the administrator.");
-        if (account.getTries() == 6) {
-            String url = "http://wontoncode.com";
-
-            String content = String.format("Hello %s,\n\n" +
-                    "Unfortunately you forgot your password. It's okay! This is what the study is for.\n" +
-                    "Your account will be unlocked in 24 hours while the administrator conducts the study.\n\n" +
-                    "Your password is: %s %s %s %s \n" +
-                    "For inquiries regarding this study, please feel free to contact\nJason Chen (jason.chen@stonybrook.edu)," +
-                    "Yang Sheng Fang (yangsheng.fang@stonybrook.edu), or Monika Tuchowska (monika.tuchowska@stonybrook.edu).",
-                    account.getUsername(), url + account.getPassword().get(0).getPath(), url + account.getPassword().get(1).getPath(),
-                    url + account.getPassword().get(2).getPath(), url + account.getPassword().get(3).getPath());
-            String subject = "Account Locked: " + account.getUsername();
-
-            try {
-                new ProcessBuilder("/scripts/email.sh", content, subject, account.getEmail()).start();
-            } catch (IOException e) {
-                logger.error("Was not able to send message for username " + account.getEmail());
-            }
-
-            throw new BadCredentialsException("Your account is locked, check your email for your password! The administrator will unlock it in 24 hours.");
-        }
         if (password == null || password.size() != account.getPassword().size()) {
             logger.info(username + " tried to submit an incorrectly sized password.");
             throw saveAttempt(account, "Password has errors");
@@ -98,6 +77,28 @@ public class CogAuthenticationProvider implements AuthenticationProvider {
     private BadCredentialsException saveAttempt(UserAccount account, String message) {
         account.setAttemptedLogin(account.getAttemptedLogin() + 1);
         account.setTries(account.getTries() + 1);
+        if (account.getTries() == 7) {
+            String url = "http://wontoncode.com";
+
+            String content = String.format("Hello %s,\n\n" +
+                    "Unfortunately you forgot your password. It's okay! This is what the study is for.\n" +
+                    "Your account will be unlocked in 24 hours while the administrator conducts the study.\n\n" +
+                    "Your password is: %s %s %s %s \n" +
+                    "For inquiries regarding this study, please feel free to contact\nJason Chen (jason.chen@stonybrook.edu)," +
+                    "Yang Sheng Fang (yangsheng.fang@stonybrook.edu), or Monika Tuchowska (monika.tuchowska@stonybrook.edu).",
+                    account.getUsername(), url + account.getPassword().get(0).getPath(), url + account.getPassword().get(1).getPath(),
+                    url + account.getPassword().get(2).getPath(), url + account.getPassword().get(3).getPath());
+            String subject = "Account Locked: " + account.getUsername();
+
+            try {
+                new ProcessBuilder("/scripts/email.sh", content, subject, account.getEmail()).start();
+            } catch (IOException e) {
+                logger.error("Was not able to send message for username " + account.getEmail());
+            }
+
+            throw new BadCredentialsException("Your account is locked, check your email for your password! The administrator will unlock it in 24 hours.");
+        }
+
         authService.updateUser(account);
         return new BadCredentialsException(message);
     }
